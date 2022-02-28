5138b0367b8c6d6a0fb999021bdc460245c06ff0
Nhat Minh <nhatminhminh57@gmail.com>
2022-02-28 23:12:24 +0700

    update 29/002/2022

--- app/src/main/java/com/example/danhbai_auto_mayvsmay/MainActivity.java
+++ app/src/main/java/com/example/danhbai_auto_mayvsmay/MainActivity.java
@@ -132,13 +132,13 @@ public class MainActivity extends AppCompatActivity  {
     }
 
     //Hàm tính thắng thua
-    private int tinhThangThua(int [] a){
+    private int tinhThangThua_of_Nguoi(int [] a){
         int nut=0;
-        if(tinhsoBaiTay(a) ==3)
+        if(tinhsoBaiTayofNguoi(a) ==3)
             nut=10;
         else {
             int temp=0;
-            for(int i=0;i<a.length;i++)
+            for(int i=0;i<a.length-3;i++)
 
                 if(a[i] %13 <10)
                     temp=temp+(a[i]%13 +1);
@@ -148,36 +148,75 @@ public class MainActivity extends AppCompatActivity  {
         }
         return nut;
     }
-    //hàm tính số nút
-    private String tinhSoNut(int[] a){
+    private int tinhThangThua_of_May(int [] a){
+        int nut=0;
+        if(tinhsoBaiTayofMay(a) ==3)
+            nut=10;
+        else {
+            int temp=0;
+            for(int i=3;i<a.length;i++)
+
+                if(a[i] %13 <10)
+                    temp=temp+(a[i]%13 +1);
+            nut=temp%10;
+            if(temp %10 ==0)
+                nut=0;
+        }
+        return nut;
+    }
+    private String tinhSoNutofNguoi(int[] a){
         String ketqua="";
-        if(tinhsoBaiTay(a) == 3)
+        if(tinhsoBaiTayofNguoi(a) == 3)
             ketqua="3 cào";
         else {
             int tongsonut=0;
-            for(int i=0;i<a.length;i++)
+            for(int i=0;i<a.length-3;i++)
                 if(a[i] %13 <10)
                     tongsonut=tongsonut+(a[i]%13 +1);
             if(tongsonut %10 ==0 )
-                ketqua ="kết quả bù, trong đó có "+tinhsoBaiTay(a)+"tây";
+                ketqua ="kết quả bù, trong đó có "+tinhsoBaiTayofNguoi(a)+"tây";
             else {
-                ketqua=(tongsonut%10)+"\tnút , "+"trong đó có:"+tinhsoBaiTay(a)+"tây";
+                ketqua=(tongsonut%10)+"\tnút , "+"trong đó có:"+tinhsoBaiTayofNguoi(a)+"tây";
+            }
+        }
+        return ketqua;
+    }
+    private String tinhSoNutofMay(int[] a){
+        String ketqua="";
+        if(tinhsoBaiTayofMay(a) == 3)
+            ketqua="3 cào";
+        else {
+            int tongsonut=0;
+            for(int i=3;i<a.length;i++)
+                if(a[i] %13 <10)
+                    tongsonut=tongsonut+(a[i]%13 +1);
+            if(tongsonut %10 ==0 )
+                ketqua ="kết quả bù, trong đó có "+tinhsoBaiTayofMay(a)+"tây";
+            else {
+                ketqua=(tongsonut%10)+"\tnút , "+"trong đó có:"+tinhsoBaiTayofMay(a)+"tây";
             }
         }
         return ketqua;
     }
 
     //hàm kiểm tra lá bài tây
-    private int tinhsoBaiTay(int [] a){
+    private int tinhsoBaiTayofNguoi(int [] a){
         int k=0;
-        for(int i=0;i<a.length;i++)
+        for(int i=0;i<a.length-3;i++)
+            if(a[i]%13 >=10 && a[i]%13 <13)
+                k++;
+        return k;
+    }
+    private int tinhsoBaiTayofMay(int [] a){
+        int k=0;
+        for(int i=3;i<a.length;i++)
             if(a[i]%13 >=10 && a[i]%13 <13)
                 k++;
         return k;
     }
     //hàm lấy 3 số ngẫu nhiên
     private int[] lay3songaunhien(int min, int max){
-        int [] baso = new int[3];
+        int [] baso = new int[6];
         int i=0;
         baso[i++] = random(min, max);
         do {
@@ -185,7 +224,7 @@ public class MainActivity extends AppCompatActivity  {
             if(!kiemTraTrung(k,baso)){
                 baso[i++]=k;
             }
-        }while (i<3);
+        }while (i<6);
         return baso;
     }
     //ham random
@@ -216,29 +255,29 @@ public class MainActivity extends AppCompatActivity  {
                 String dinhdang= String.format("%02d:%02d:%02d", TimeUnit.MILLISECONDS.toHours(miligiay),TimeUnit.MILLISECONDS.toMinutes(miligiay) - TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(miligiay)), TimeUnit.MILLISECONDS.toSeconds(miligiay) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(miligiay)));
                 countdownTimerText.setText(dinhdang);
 
-                int [] value_nguoi= lay3songaunhien(0,51);
+                int [] value= lay3songaunhien(0,51);
 
                 YoYo.with(Techniques.SlideInRight).duration(1000).repeat(0).playOn(hinh1);
                 YoYo.with(Techniques.SlideInRight).duration(1200).repeat(0).playOn(hinh2);
                 YoYo.with(Techniques.SlideInRight).duration(1400).repeat(0).playOn(hinh3);
-                hinh1.setImageResource(manghinhbai[value_nguoi[0]]);
+                hinh1.setImageResource(manghinhbai[value[0]]);
 
-                hinh2.setImageResource(manghinhbai[value_nguoi[1]]);
-                hinh3.setImageResource(manghinhbai[value_nguoi[2]]);
-                String kq_nguoi=tinhSoNut(value_nguoi);
+                hinh2.setImageResource(manghinhbai[value[1]]);
+                hinh3.setImageResource(manghinhbai[value[2]]);
+                String kq_nguoi=tinhSoNutofNguoi(value);
                 textViewKetQua_nguoi.setText(kq_nguoi);
 
-                int [] value_may= lay3songaunhien(0,51);
+
                 YoYo.with(Techniques.SlideInRight).duration(1000).repeat(0).playOn(hinh4);
                 YoYo.with(Techniques.SlideInRight).duration(1200).repeat(0).playOn(hinh5);
                 YoYo.with(Techniques.SlideInRight).duration(1400).repeat(0).playOn(hinh6);
-                hinh4.setImageResource(manghinhbai[value_may[0]]);
-                hinh5.setImageResource(manghinhbai[value_may[1]]);
-                hinh6.setImageResource(manghinhbai[value_may[2]]);
-                String kq_may=tinhSoNut(value_may);
+                hinh4.setImageResource(manghinhbai[value[3]]);
+                hinh5.setImageResource(manghinhbai[value[4]]);
+                hinh6.setImageResource(manghinhbai[value[5]]);
+                String kq_may=tinhSoNutofMay(value);
                 textviewKetQua_may.setText(kq_may);
-                int lanthangofnguoi=tinhThangThua(value_nguoi);
-                int lanthangofmay= tinhThangThua(value_may);
+                int lanthangofnguoi=tinhThangThua_of_Nguoi(value);
+                int lanthangofmay= tinhThangThua_of_May(value);
                 if(lanthangofnguoi>lanthangofmay){
                     Toast.makeText(MainActivity.this, "Người chơi thắng", Toast.LENGTH_SHORT).show();
                     Source_nguoi++;
